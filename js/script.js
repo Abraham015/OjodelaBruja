c096ec584c4d4835be9a60aad56a85bfcf27fa55
Abraham015 <ahernadeza1607@alumno.ipn.mx>
2022-12-02 12:56:24 -0600

    Cambio en el tablero

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 function tarot(){
     swal.fire({
       title: 'Tarot',
-      text: 'Esta es una prueba',
-      imageUrl: './images/el tonto.jpeg',
+      //text: 'Esta es una prueba',
+      html: '<ol class="list-group list-group-numbered"><li class="list-group-item">Un arte de adivinación.</li>      <li class="list-group-item">Una visión simbólica del Cosmos.</li>      <li class="list-group-item">Un compendio de conocimientos esotéricos.</li>      <li class="list-group-item">Un mapa de la psique.</li>      <li class="list-group-item">Un camino de crecimiento espiritual.</li>      <li class="list-group-item">El legado de otras civilizaciones.</li>      <li class="list-group-item">Una representación simbólica del Árbol de la Vida.</li>     <li class="list-group-item">Una ilustración de las fuerzas de la Naturaleza.</li>      <li class="list-group-item">Un instrumento de autodescubrimiento.</li>  </ol>',      imageUrl: './images/el tonto.jpeg',
       imageWidth: 76,
       imageHeight: 152,
       padding: 10,
+      cancelButtonColor: '#d33',
       animation: true,
     });
   }
