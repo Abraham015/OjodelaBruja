d9d37da901cad5dfb7736c1b8856a8954936e7dc
Abraham015 <ahernadeza1607@alumno.ipn.mx>
2022-12-02 12:48:56 -0600

    Cambio en el tablero

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function tarot(){
+    swal.fire({
+      title: 'Tarot',
+      text: 'Esta es una prueba',
+      imageUrl: './images/el tonto.jpeg',
+      imageWidth: 76,
+      imageHeight: 152,
+      padding: 10,
+      animation: true,
+    });
+  }
